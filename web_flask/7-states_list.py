206899bf29e13ef01c53cb6a68deba05fcc75934
Mikaelia <mikaelagurney@gmail.com>
2018-08-15 22:43:01 +0000

    Pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,9 +12,11 @@ def state_list():
     state_list = [v for v in states.values()]
     return render_template('7-states_list.html', states=state_list)
 
+
 @app.teardown_appcontext
 def teardown(value):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
