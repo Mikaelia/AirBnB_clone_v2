02e0106061bb82ec4d6b95482191472a361b17f0
Mikaelia <mikaelagurney@gmail.com>
2018-08-15 19:15:21 +0000

    Edit 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask
+"""
+Initializes flask app to listen on 0.0.0.0:5000
+"""
 
 app = Flask(__name__)
 
@@ -8,4 +11,6 @@ app = Flask(__name__)
 def index():
     return "Hello HBNB"
 
-app.run(host= '0.0.0.0')
+
+if __name__ == "__main__":
+    app.run()
