8d914a60e773a09664a7a5e2cbbd09b56f791579
Mikaelia <mikaelagurney@gmail.com>
2018-08-16 03:25:36 +0000

    Add task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -14,17 +14,18 @@ def cities_by_state():
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_by_id(id):
-    state_found = None
     states = storage.all('State')
-    for v in states.values():
-        if id == v.id:
-            state_found = v
     cities = storage.all('City')
+    amenities = storage.all('Amenity')
+    
+    state_list = [v for v in cities.values() if v.state_id == id]
     city_list = [v for v in cities.values() if v.state_id == id]
+    amenity_list = [v for v in cities.values() if v.state_id == id]
     return render_template(
-        '9-states.html',
-        state_found=state_found,
-        cities=city_list, states=None)
+        '10-hbnb_filters.html',
+        states=state_list,
+        cities=city_list,
+        amenities=amenity_list)
 
 
 @app.teardown_appcontext
