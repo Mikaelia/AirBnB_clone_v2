fd4eb1a3d89a2974ca4d0b18891696b646337b39
Mikaelia <mikaelagurney@gmail.com>
2018-08-16 16:55:48 +0000

    File updates

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+"""
+Main view functions
+"""
 from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
 
-
-@app.route('/states', strict_slashes=False)
-def cities_by_state():
-    states = storage.all('State')
-    state_list = [v for v in states.values()]
-    return render_template('9-states.html', states=state_list)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
+@app.route('/hbnb_filters', strict_slashes=False)
+def display_static():
     states = storage.all('State')
     cities = storage.all('City')
     amenities = storage.all('Amenity')
-    
-    state_list = [v for v in cities.values() if v.state_id == id]
-    city_list = [v for v in cities.values() if v.state_id == id]
-    amenity_list = [v for v in cities.values() if v.state_id == id]
+
+    state_list = [v for v in states.values()]
+    city_list = [v for v in cities.values()]
+    amenity_list = [v for v in amenities.values()]
     return render_template(
         '10-hbnb_filters.html',
         states=state_list,
